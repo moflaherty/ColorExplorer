49f208cbbc1b7306fd718697454814828621c6d0
moflaherty <moflaherty@eInvasion.com>
2014-09-20 15:03:54 -0400

    Added help comments to application

--- ui.R
+++ ui.R
@@ -22,7 +22,9 @@ shinyUI(fluidPage(
                   value = 25),
       selectInput("color", "Choose a color theme:", multiple = FALSE,
                   choices = c("cm", "heat", "rainbow", "terrain", "topo")),
-      helpText("This example uses rnorm() for data generation.")
+      helpText("This example uses rnorm() for data generation."),
+      helpText("Color Explorer Help"),
+      helpText("The Chart Type drop-down is what type of chart is displayed. The Number of Bars/Slices control controls the number of elements displayed in the chart. The Color Theme drop-down controls what color theme to display. If you have any problems what-so-ever, you may contact technical support at michael@oflaherty.com.")
     ),
     
     # show the chart
